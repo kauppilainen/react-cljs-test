b69c65147cd1b9f95b1b15b27b2d4ed08c2a7437
Felix Alm <fel@juxt.pro>
2024-09-14 13:46:48 +0100

    Add example of http request to repl process

--- src/counter.cljs
+++ src/counter.cljs
@@ -4,10 +4,14 @@
   (inc n))
 
 (comment
+  ;; Bind n to 1 in REPL from generated javascript
+  ;; Request fails and the Squint REPL dies but
+  ;; this is along the lines of what I'm thinking
+  (def n 1)
   (js/fetch "http://localhost:1888"
             {:method "POST"
              :body (js/JSON.stringify
-                    {:op "eval"
-                     :ns "foo"
-                     :code (str `(def n ,n))})})
+                    {:ns "counter"
+                     :op "eval"
+                     :code "(def n 1)"})})
   )
