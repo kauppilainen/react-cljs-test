7d16a859908062620a705befc808733d53159998
Felix Alm <fel@juxt.pro>
2024-09-15 20:21:15 +0100

    Remove comment block

--- src/counter.cljs
+++ src/counter.cljs
@@ -2,16 +2,3 @@
 
 (defn customInc [n]
   (inc n))
-
-(comment
-  ;; Bind n to 1 in REPL from generated javascript
-  ;; Request fails and the Squint REPL dies but
-  ;; this is along the lines of what I'm thinking
-  (def n 1)
-  (js/fetch "http://localhost:1888"
-            {:method "POST"
-             :body (js/JSON.stringify
-                    {:ns "counter"
-                     :op "eval"
-                     :code "(def n 1)"})})
-  )
